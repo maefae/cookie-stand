e2ce88203cf934ec57f7bc15e78acedc8b4c2896
Megan Faith Seibert-Hughes <meganfaithhughes@gmail.com>
2022-08-22 19:14:25 -0500

    added js, html, css, and jpg files"

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
+'use strict';
 
+let storeHours = ["6am", "7am", '8am', '9am', '10am', "11am", "12am", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm"];
+
+let seattle = {
+  Location:'Seattle',
+  minCustomersPerHour:23,
+  maxCustomersPerHour:65,
+  avgSales:6.3,
+  hourlySales:[]
+};
+//for loop that iterates storeHours array. Random number between min and max multiply it by average and push it into hourly sales
+console.log(seattle);
+console.log(seattle.hourlySales)
+
+return Math.floor(Math.random() * max);
+
+for (let i = 0; i < 14;i++) {
+
+}
